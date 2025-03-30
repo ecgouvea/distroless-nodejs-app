7345e670303795d1747631a5decfd34b8ebe04b5
Eduardo Gouvea <eduardo.gouvea@bbce.com.br>
2025-03-29 21:16:04 -0300

    Add log

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!' + new Date() + '\n');
+  let response = 'Hello, World!' + new Date();
+  console.log(response);
+  res.end(response);
 });
 
 server.listen(port, hostname, () => {
